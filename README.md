951a07abbfc723b0b3091f4b2b6d0e2cf822c38e
Secwayz <timurpodshivalov@yandex.ru>
2020-11-29 13:27:10 +0000

    add users, user, avatar

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # xenforo
 XenForo API library
 
-#### Api.find_name("username")
-Get user object by username, return [type user](https://xenforo.com/community/pages/api-endpoints/#type_User) or None
+#### Api.find_name(username)
+Get user object by username
+:return: [type user](https://xenforo.com/community/pages/api-endpoints/#type_User) || None
 
-#### Api.find_email("email")
-Get user object by email, return [type user](https://xenforo.com/community/pages/api-endpoints/#type_User) or None
+#### Api.find_email(email)
+Get user object by email
+:return: list, [type user](https://xenforo.com/community/pages/api-endpoints/#type_User) || None
+|
+#### Api.users(page)
+Get dict users of PAGE
+:return: dict, username:id
+
+#### Api.user(username, password, email)
+Create user
+:return: boolean
+
+#### Api.avatar(username, path_to_file)
+Add user avatar from path
+:return: booleand
--- xenforo.py
+++ xenforo.py
@@ -11,13 +11,14 @@ xf = Api("token")
 """
 
 class Api:
-	def __init__(self, url, api_key):
+	def __init__(self, url, api_key, api_user=1):
 		self.url = url
 		self.api_key = api_key
+		self.api_user = api_user
 
 		self.headers = {
 			'XF-Api-Key': self.api_key,
-			'XF-Api-User': '1',
+			'XF-Api-User': str(self.api_user),
 			'Content-Type': 'application/x-www-form-urlencoded'
 		}
 
@@ -42,4 +43,42 @@ class Api:
 
 		except:
 			return None
+	
 
+	def users(self, page=1):
+		users = {}
+		url = self.url + f"/api/users/?page={page}"
+
+		try:
+			response = requests.get(url=url, headers=self.headers)
+
+			for item in response.json()["users"]:
+				users[item["username"]] = item["user_id"]
+
+		except:
+			pass
+
+		return users
+
+
+	def user(self, username, password, email):
+		url = self.url + f"/api/users/?username={username}&password={password}&email={email}"
+
+		try:
+			response = requests.post(url=url, headers=self.headers)
+			return response.json()["success"]
+
+		except:
+			return False
+
+
+	def avatar(self, username, file):
+		files = {'avatar':  open(file, 'rb')}
+		url = self.url + f"/api/users/{username}/avatar"
+
+		try:
+			response = requests.post(url=url, files=files, headers=self.headers)
+			return response.json()["success"]
+
+		except:
+			return False
